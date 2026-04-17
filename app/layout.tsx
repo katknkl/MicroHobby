4d99a65718a4dd48b1d6ab1a321fbb7edae713b1
msimons7 <mckennajsimons@gmail.com>
2026-04-17 15:38:57 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,3 +64,15 @@ export default function RootLayout({
     </html>
   )
 }
+import { ThemeProvider } from "next-themes";
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
