a7a7e579aadf6d73f373f99461838bf911e7f6a3
jaysontramm <jayson.tram@sjsu.edu>
2026-04-17 19:34:39 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,15 +64,4 @@ export default function RootLayout({
     </html>
   )
 }
-import { ThemeProvider } from "next-themes";
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
-}
+
